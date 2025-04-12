f297a409cf9eac1c134d9b0bb6753a496657da7d
Palfi Aron <h376388@stud.u-szeged.hu>
2025-04-12 09:52:29 +0200

    keret létrehozása

---
+++ src/app/pages/logine/logine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logine',
+  imports: [],
+  templateUrl: './logine.component.html',
+  styleUrl: './logine.component.scss'
+})
+export class LogineComponent {
+
+}
