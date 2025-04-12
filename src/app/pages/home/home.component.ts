fec29e941bb3721729b6f3cca679792965aab24f
Palfi Aron <h376388@stud.u-szeged.hu>
2025-04-12 20:33:44 +0200

    test

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, // Ha standalone komponens, akkor szükséges
+  imports: [
+    MatButtonModule, // Angular Material gombok
+    MatIconModule,   // Angular Material ikonok
+    RouterModule     // RouterLink használatához
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
