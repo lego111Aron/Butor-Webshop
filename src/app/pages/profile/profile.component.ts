4e62d3a1f4ff7f23b3dec8f2bcfee6bb11fd8635
Palfi Aron <h376388@stud.u-szeged.hu>
2025-04-13 09:26:02 +0200

    A profil oldal bal oldala

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card'; // <== MatCard hozzáadása
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatCardModule // <== Fontos!
+  ],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  username: string = 'Béla Kovács';
+
+  get usernameInitial(): string {
+    return this.username.charAt(0).toUpperCase();
+  }
+}
